9fb76b0baf74f69bdc557d8ac424cecc9570048e
Rodrigo Riveros <riverosdev@gmail.com>
2024-03-02 18:02:50 -0300

    agrego contenido y estilos en el componente HeaderNav

--- src/components/layout/HeaderNav.jsx
+++ src/components/layout/HeaderNav.jsx
@@ -1,12 +1,29 @@
+import { NavLink } from "react-router-dom"
+import './Header.css'
 
 export const HeaderNav = () => {
   return (
-    <header>
-        <h1>Rodrigo Riveros WEB</h1>
+    <header className="header">
+        <div className="logo">
+            <span>R</span>
+            <h2>Rodrigo Riveros WEB</h2>
+        </div>
         <nav>
             <ul>
                 <li>
-                    
+                    <NavLink to="/inicio">Inicio</NavLink>
+                </li>
+                <li>
+                    <NavLink to='/portafolio'>Portafolio</NavLink>
+                </li>
+                <li>
+                    <NavLink to='/servicios'>Servicios</NavLink>
+                </li>
+                <li>
+                    <NavLink to='/curriculum'>Curriculum</NavLink>
+                </li>
+                <li>
+                    <NavLink to='/contacto'>Contacto</NavLink>
                 </li>
             </ul>
         </nav>
